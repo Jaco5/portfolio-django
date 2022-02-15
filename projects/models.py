bcf0b315e7eade14bb25d3af79db3a42c3f0b8b1
Jaco5 <jacobartz1987@gmail.com>
2022-02-14 19:35:50 -0800

    project model added url, added one proj to db

--- projects/models.py
+++ projects/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    technology = models.CharField(max_length=100)
+    hosted = models.URLField(default="www.github.com")
+#    image = models.FilePathField(path="/img")
